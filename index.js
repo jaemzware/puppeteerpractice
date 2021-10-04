84245233b78fc2da99834dc14fcb5c8dc77b40cc
JAMES ARASIM <jaemzware@hotmail.com>
2021-10-04 09:54:18 -0700

    looking good. i like puppeteer a lot

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const CREDS = require('./creds');
 
 
 async function run() {
+
+    //LOGIN
     const USERNAME_SELECTOR="#login_field";
     const PASSWORD_SELECTOR="#password";
     const BUTTON_SELECTOR="input[value='Sign in']";
@@ -10,22 +12,22 @@ async function run() {
         headless: false
     });
     const page = await browser.newPage();
-
     await page.goto('https://github.com/login');
-    await page.screenshot({ path: 'screenshots/github.png' });
-
-
     await page.click(USERNAME_SELECTOR);
     await page.keyboard.type(CREDS.username);
-
     await page.click(PASSWORD_SELECTOR);
     await page.keyboard.type(CREDS.password);
-
     await page.click(BUTTON_SELECTOR);
+    await page.waitForSelector("#repos-container > h2 > a");
 
-    await page.waitForNavigation();
-
-    browser.close();
+    //SEARCH
+    const SEARCHBOX_SELECTOR = "#js-pjax-container > div > div.col-12.col-md-3.float-left.px-md-2 > nav.menu.border.d-none.d-md-block > a.menu-item.selected";
+    const userToSearch = 'jimarasim';
+    const searchUrl = `https://github.com/search?q=${userToSearch}&type=Users&utf8=%E2%9C%93`;
+    await page.goto(searchUrl);
+    await page.waitForSelector(SEARCHBOX_SELECTOR);
+    await page.screenshot({ path: 'screenshots/search.png' });
+    await browser.close();
 }
 
 run();
